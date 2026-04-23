be7a3d7eeff316ddffcff49c11d5755e45f29413
binayak57 <binayak4370@gmail.com>
2026-04-23 19:44:59 +0530

    Remove test files

--- test.js
+++
@@ -1,6 +0,0 @@
-const https = require('https');
-https.get('https://www.omdbapi.com/?apikey=thewdb&t=interstellar', (res) => {
-  let data = '';
-  res.on('data', d => data += d);
-  res.on('end', () => console.log(data));
-});
